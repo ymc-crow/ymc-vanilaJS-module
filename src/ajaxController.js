06904e9fbf1fabde3857664eb3476e939b711de6
ymc-crow <ymch65@naver.com>
2021-02-01 23:00:32 +0900

    fetchcontroller develope

--- src/ajaxController.js
+++ src/ajaxController.js
@@ -1,3 +1,21 @@
-function ajaxCall({url, method, data}) {
-
+function ajaxController({url, method='GET', data}) {
+    const abortController = new AbortController();
+    return {
+        call: async () => {
+            try {
+                const response = await fetch(url, {
+                    signal: abortController.signal,
+                    method,
+                    headers: {
+                        'Content-Type': 'application/json;charset=utf-8'
+                    },
+                    body: JSON.stringify(data)
+                });
+                return await response.json();
+            }catch(e) {
+                return e;
+            }
+        },
+        abort: abortController.abort
+    };
 };
