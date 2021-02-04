4fba1f485ddbfd92f9182021a7ba57a7be7eff37
ymcgood <ymcgood@MAC-YMCGOOD.local>
2021-02-04 10:31:02 +0900

    add server.js consoleLog

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(8080, function(){
     console.log('Server running on 8080...');
-    console.log()
+    console.log('Go to http://localhost:8080/src/test.html')
 });
